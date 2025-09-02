80448e55a56e26e8d91b81b0bcb2803c8670c0a5
[dyad] <riotbrendan@gmail.com>
2025-09-02 18:59:09 +0700

    Reverted all changes back to version 4f802b77bfd9236d037dc6e3923b1eef83eb302b

--- src/utils/sound.ts
+++
@@ -1,6 +0,0 @@
-export const playClickSound = () => {
-  const audio = new Audio('/attibute_click.mp3');
-  audio.play().catch(error => {
-    console.log("Sound play was prevented by the browser:", error);
-  });
-};
