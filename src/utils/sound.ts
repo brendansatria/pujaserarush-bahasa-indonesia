33d11b00de6c8e6637077dfa461f40c78bed5f78
[dyad] <riotbrendan@gmail.com>
2025-09-02 19:01:10 +0700

    [dyad] Fix sound file loading error - wrote 1 file(s), renamed 1 file(s)

--- src/utils/sound.ts
+++ src/utils/sound.ts
@@ -1,7 +1,7 @@
 export const playClickSound = () => {
   try {
     // The sound file is in the public directory
-    const audio = new Audio('/sounds/attibute_click.mp3');
+    const audio = new Audio('/sounds/attribute_click.mp3');
     audio.play();
   } catch (error) {
     console.error("Error playing sound:", error);
